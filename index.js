89186651933ad1cc37987a06a11170a78e0ab41e
Jesse Wei <business.jessecreatives@gmail.com>
2022-04-21 16:40:42 +0900

    add placeholder and styles

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const items = document.querySelectorAll(".draggable")
 let draggingEl
+let elementWidth
+let elementHeight
 let clientX
 let clientY
+let placeholder
+let isDraggingStarted = false
 
 const handleMouseDown = (event) => {
   draggingEl = event.target
 
+  elementWidth = draggingEl.offsetWidth
+  elementHeight = draggingEl.offsetHeight
   clientX = event.clientX
   clientY = event.clientY
 
@@ -14,6 +20,15 @@ const handleMouseDown = (event) => {
 }
 
 const handleMouseMove = (event) => {
+  if (!isDraggingStarted) {
+    isDraggingStarted = true
+    // placeholder
+    placeholder = document.createElement("div")
+    placeholder.classList.add("placeholder")
+    placeholder.style.width = `${elementWidth}px`
+    placeholder.style.height = `${elementHeight}px`
+    draggingEl.parentNode.insertBefore(placeholder, draggingEl.nextSibling)
+  }
   draggingEl.style.position = "absolute"
   draggingEl.style.left = `${draggingEl.offsetLeft + event.clientX - clientX}px`
   draggingEl.style.top = `${draggingEl.offsetTop + event.clientY - clientY}px`
@@ -24,8 +39,11 @@ const handleMouseMove = (event) => {
 
 const handleMouseUp = () => {
   draggingEl = null
+  elementWidth = null
+  elementHeight = null
   clientX = null
   clientY = null
+  isDraggingStarted = false
 
   document.removeEventListener("mousemove", handleMouseMove)
   document.removeEventListener("mouseup", handleMouseUp)
