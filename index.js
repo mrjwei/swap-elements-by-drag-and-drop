a442c8f120e6e08dcd02861c980256f33690b353
Jesse Wei <business.jessecreatives@gmail.com>
2022-04-24 10:02:28 +0900

    handle touch

--- index.js
+++ index.js
@@ -134,8 +134,90 @@ function swapElementsByDragAndDrop() {
     document.removeEventListener("mouseup", handleMouseUp)
   }
 
+  const handleTouchStart = (event) => {
+    draggingEl = event.target
+
+    const touch = event.changedTouches[0]
+
+    elementWidth = draggingEl.offsetWidth
+    elementHeight = draggingEl.offsetHeight
+    elementY = draggingEl.offsetTop
+    clientX = touch.clientX
+    clientY = touch.clientY
+
+    draggingEl.style.color = "red"
+    draggingEl.style.borderColor = "red"
+
+    document.addEventListener("touchmove", handleTouchMove)
+    document.addEventListener("touchend", handleTouchUp)
+  }
+
+  const handleTouchMove = (event) => {
+    const touch = event.changedTouches[0]
+
+    if (!isDraggingStarted) { // only create placeholder once
+      isDraggingStarted = true
+
+      placeholder = createPlaceholder()
+      configPlaceholder(placeholder, elementWidth, elementHeight)
+      insertPlaceholder(draggingEl, placeholder)
+    }
+    draggingEl.style.position = "absolute"
+    draggingEl.style.left = `${draggingEl.offsetLeft + touch.clientX - clientX}px`
+    draggingEl.style.top = `${draggingEl.offsetTop + touch.clientY - clientY}px`
+
+    swapElements(
+      items,
+      draggingEl,
+      elementY,
+      placeholder
+    )
+
+    clientX = touch.clientX
+    clientY = touch.clientY
+  }
+
+  const handleTouchUp = (event) => {
+    if (isDraggingStarted) {
+      draggingEl.style.removeProperty("top")
+      draggingEl.style.removeProperty("left")
+      draggingEl.style.removeProperty("position")
+
+      // fix draggingEl at placeholder's current position
+      draggingEl.parentNode.insertBefore(draggingEl, placeholder)
+
+      placeholder && draggingEl.parentNode.removeChild(placeholder)
+      placeholder = null
+
+      isDraggingStarted = false
+    }
+
+    elementWidth = null
+    elementHeight = null
+    elementY = null
+    clientX = null
+    clientY = null
+
+    /**
+     * Falls back to styles specified in css and
+     * hover styles applies properly.
+     * Using:
+     *   draggingEl.style.color = "grey"
+     *   draggingEl.style.borderColor = "grey"
+     * will override hover styles.
+    */
+    draggingEl.style.removeProperty("color")
+    draggingEl.style.removeProperty("border-color")
+
+    draggingEl = null
+
+    document.removeEventListener("touchmove", handleTouchMove)
+    document.removeEventListener("touchend", handleTouchUp)
+  }
+
   Array.from(items).forEach(item => {
     item.addEventListener("mousedown", handleMouseDown)
+    item.addEventListener("touchstart", handleTouchStart)
   })
 }
 
